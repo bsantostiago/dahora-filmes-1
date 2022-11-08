242a00999e780709e0c0ca2ca24546fe1f700dbf
tiagoprogwebt22 <tiago.tecinternet22@gmail.com>
2022-11-08 11:02:26 -0300

    Instalação libs do react-navigation

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
 import { StatusBar, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import Favoritos from "./src/screens/Favoritos";
 import FormBusca from "./src/screens/FormBusca";
 import Home from "./src/screens/Home";
@@ -6,12 +9,21 @@ import Privacidade from "./src/screens/Privacidade";
 import Sobre from "./src/screens/Sobre";
 
 const App = () => {
+  /* Inicializando através de uma constante
+  o gerenciador de navegação Stack (pilha de telas) */
+  const Stack = createNativeStackNavigator();
+
   return (
-    /* opções para o barStyle: 
-    dark-content, light-content ou default */
     <>
       <StatusBar />
-      <Home />
+
+      {/* O NavigationContainer deve envolver todas as telas
+      navegáveis do nosso App. */}
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen component={Home} name="Home" />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 };
