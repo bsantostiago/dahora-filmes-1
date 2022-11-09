73e6d3b3fb37bd0e5395b186aa56ab8c8c6d0b44
tiagoprogwebt22 <tiago.tecinternet22@gmail.com>
2022-11-09 11:22:47 -0300

    Correção exercício UI

--- src/screens/FormBusca.js
+++ src/screens/FormBusca.js
@@ -1,13 +1,53 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import {
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 const FormBusca = () => {
   return (
-    <SafeAreaView>
-      <Text>FormBusca</Text>
+    <SafeAreaView style={estilos.container}>
+      <Text style={estilos.texto}>
+        Star Trek? O Poderoso Chefão? A trilogia Senhor dos Anéis?
+      </Text>
+      <Text style={estilos.texto}>
+        Localiza um filme que você viu ou gostaria de ver!
+      </Text>
+      <View style={estilos.viewForm}>
+        <Ionicons name="film" size={44} />
+        <TextInput placeholder="Digite o filme..." style={estilos.campo} />
+      </View>
+      <Button title="Procurar" color="#5451a6" />
     </SafeAreaView>
   );
 };
 
 export default FormBusca;
 
-const styles = StyleSheet.create({});
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  texto: {
+    paddingVertical: 8,
+  },
+  viewForm: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 16,
+  },
+  campo: {
+    borderWidth: 1,
+    padding: 8,
+    flex: 0.9,
+  },
+  botao: {
+    backgroundColor: "red",
+    borderWidth: 2,
+  },
+});
