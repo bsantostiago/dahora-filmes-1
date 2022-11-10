38de990a6e8cdc36b7ff92d797dded48aec169a9
tiagoprogwebt22 <tiago.tecinternet22@gmail.com>
2022-11-10 12:22:26 -0300

    Update Resultados.js

--- src/screens/Resultados.js
+++ src/screens/Resultados.js
@@ -16,7 +16,7 @@ const Resultados = ({ route }) => {
       try {
         const resposta = await api.get("/search/movie", {
           params: {
-            api_key: "a2663600caa2483f5ddd512c577b4101",
+            api_key: "COLOQUE_SUA_API_KEY",
             language: "pt-BR",
             query: filme,
             include_adult: false,
