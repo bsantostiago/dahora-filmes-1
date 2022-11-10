f696009e0b9c67e730877a266cf2c5a9ac65d2a2
tiagoprogwebt22 <tiago.tecinternet22@gmail.com>
2022-11-10 12:20:16 -0300

    Correção caminho import api

--- src/screens/Resultados.js
+++ src/screens/Resultados.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import api from "./services/api";
+import api from "../services/api";
 
 const Resultados = ({ route }) => {
   /* Usamos a prop route (do React Navigation) para
