585b123f27a6d8110e95980958de1d6f1c5d99b3
tiagoprogwebt22 <tiago.tecinternet22@gmail.com>
2022-11-18 11:36:41 -0300

    Verificação de filme já salvo

--- src/components/CardFilme.js
+++ src/components/CardFilme.js
@@ -31,6 +31,18 @@ const CardFilme = ({ filme }) => {
       listaDeFilmes = [];
     }
 
+    /* Etapa de verificação de filme já salvo */
+
+    /* Para cada filme existente na listaDeFilmes (se existir) */
+    for (let filmeExistente in listaDeFilmes) {
+      /* Verificamos se o id do filme existente é igual ao id do
+      filme do card (que está na tela) */
+      if (listaDeFilmes[filmeExistente].id == filme.id) {
+        Alert.alert("Ops!", "Você já salvou este filme!");
+        return;
+      }
+    }
+
     // 4) Adicionamos os dados do filme na lista (array)
     listaDeFilmes.push(filme);
 
